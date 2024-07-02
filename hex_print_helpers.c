ee05dfc189b356a41d676261acdfa2d78fc99b89
Rodrigo Hoffmann <rohoffma@c3a9c2.42berlin.de>
2024-07-02 17:42:34 +0200

    ft_printf: Function finished passing the norm, my tests and francinette tests

--- hex_print_helpers.c
+++ hex_print_helpers.c
@@ -6,7 +6,7 @@
 /*   By: rohoffma <rohoffma@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 18:56:20 by rohoffma          #+#    #+#             */
-/*   Updated: 2024/06/14 13:15:01 by rohoffma         ###   ########.fr       */
+/*   Updated: 2024/07/02 17:40:17 by rohoffma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,27 @@
 int	ft_puthex_lower(unsigned int nb)
 {
 	char	*hex_digits;
+	int		bytes_written;
 
+	bytes_written = 0;
 	hex_digits = "0123456789abcdef";
-	if (nb > 0)
-		return (ft_puthex_lower(nb / 16) + ft_putchar(hex_digits[(nb % 16)]));
-	return (0);
+	if (nb >= 16)
+		bytes_written += ft_puthex_lower(nb / 16);
+	bytes_written += ft_putchar(hex_digits[(nb % 16)]);
+	return (bytes_written);
 }
 
 int	ft_puthex_upper(unsigned int nb)
 {
 	char	*hex_digits;
+	int		bytes_written;
 
+	bytes_written = 0;
 	hex_digits = "0123456789ABCDEF";
-	if (nb > 0)
-		return (ft_puthex_upper(nb / 16) + ft_putchar(hex_digits[(nb % 16)]));
-	return (0);
+	if (nb >= 16)
+		bytes_written += ft_puthex_upper(nb / 16);
+	bytes_written += ft_putchar(hex_digits[(nb % 16)]);
+	return (bytes_written);
 }
 
 int	ft_long_to_puthex(unsigned long nb)
